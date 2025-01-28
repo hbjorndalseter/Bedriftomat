ce2385a0622f6790463c2601a3aacff5976be720
hjalmabj <hjalmabj@stud.ntnu.no>
2025-01-28 20:23:46 +0100

    Hentet bedrifter og bilder ++

---
+++ bedriftomat/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import './styling/Header.css';
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <img src="/src/assets/images/IVDagene.png" alt="IVDagene" className="header-logo" />
+        </header>
+    );
+};
+
+export default Header;
--- bedriftomat/src/main.tsx
+++ bedriftomat/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Header from './components/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div>
+      <Header />
+      <App />
+    </div>
   </StrictMode>,
 )
