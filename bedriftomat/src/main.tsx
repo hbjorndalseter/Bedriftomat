fdd555e22b0af971c304d1efeea22b7366001de5
hjalmabj <hjalmabj@stud.ntnu.no>
2025-01-28 20:52:51 +0100

    satt opp struktur og eksempel

--- bedriftomat/src/main.tsx
+++ bedriftomat/src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.tsx'
-import Header from './components/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <div>
-      <Header />
+  <StrictMode>      
       <App />
-    </div>
   </StrictMode>,
 )
