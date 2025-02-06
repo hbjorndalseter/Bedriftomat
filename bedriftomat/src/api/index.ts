559e3062eff20db9811ce24c4ee90b702018ef2f
Åmund <aamgrim@gmail.com>
2025-02-06 12:50:28 +0100

    comments ++

--- bedriftomat/src/api/index.ts
+++ bedriftomat/src/api/index.ts
@@ -1,22 +1,35 @@
 import express, {Request, Response} from 'express';
 import fs from "fs";
+import cors from 'cors';
 import { Company } from "../types";
 const app = express();
 
-app.get("/test", (req: Request, res: Response) => {
-    console.log("Siu")});
+// Middleware
+app.use(cors({ origin: 'http://localhost:5173'})); // Allow cross-origin requests
+app.use(express.json())
 
+app.get("/test", (req: Request, res: Response) => {
+    res.send("You clicked a button.")})
+
+// Hent antall bedrifter fra companies.json
 app.get("/numCompanies", (req: Request, res: Response) => {
-    // let numberOfCompanies : number;
-    const rawData = fs.readFileSync("bedriftomat/companies.json","utf-8");
-    const jsonData = JSON.parse(rawData)
-    console.log(jsonData);
+    try {
+        const rawData = fs.readFileSync("companies.json","utf-8");
+        const jsonData = JSON.parse(rawData)
+        const companies : Company[] = jsonData.companies;
+        res.send({count: companies.length}) // Send antall bedrifter til klienten
+    } catch (error) {
+        res.status(500).json({error: "Failed to load companies data."})
+    }
 })
 
-app.get("/randomQuestions",)
+app.get("/randomQuestions", (req: Request, res: Response) => {})
 
-app.post("/results")
+app.post("/results", (req: Request, res: Response) => {})
 
+// Serveren er på port 3000 (clienten er på port 5173)
 app.listen(3000, () =>{
     console.log("Lytter til en port.")
 })
+
+module.exports = app
--- bedriftomat/src/pages/StartPage.tsx
+++ bedriftomat/src/pages/StartPage.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
+
 export default function StartPage() {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/numCompanies')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error(error));
+    }, []);
+
+    const handleClick = () => {
+        fetch('http://localhost:3000/test')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+
     return (
         <div className="flex flex-col justify-center max-w-lg">
             <h1>Tittel: Bedriftomat</h1>
             <p>Info om bedriftomaten...</p>
-            <button>Start</button>
+            {data && <p>Antall bedrifter: {data.count}</p>}
+            <button onClick={handleClick}>Start</button>
         </div>
     )
 }
