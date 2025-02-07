6cc00472b5ee99f873130f8022268392c6b4a9f0
Åmund <aamgrim@gmail.com>
2025-02-07 16:40:18 +0100

    got endpoints to work

--- bedriftomat/src/api/index.ts
+++ bedriftomat/src/api/index.ts
@@ -1,34 +1,36 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import fs from "fs";
 import cors from 'cors';
 import { Company } from "../types";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173'})); // Allow cross-origin requests
+app.use(cors({ origin: 'http://localhost:5173' })); // Allow cross-origin requests
 app.use(express.json())
 
+// Test endepunkt
 app.get("/test", (req: Request, res: Response) => {
-    res.send("You clicked a button.")})
+    res.json({ text: "You clicked a button." });
+})
 
 // Hent antall bedrifter fra companies.json
 app.get("/numCompanies", (req: Request, res: Response) => {
     try {
-        const rawData = fs.readFileSync("companies.json","utf-8");
+        const rawData = fs.readFileSync("companies.json", "utf-8");
         const jsonData = JSON.parse(rawData)
-        const companies : Company[] = jsonData.companies;
-        res.send({count: companies.length}) // Send antall bedrifter til klienten
+        const companies: Company[] = jsonData.companies;
+        res.json({ count: companies.length }) // Send antall bedrifter til klienten
     } catch (error) {
-        res.status(500).json({error: "Failed to load companies data."})
+        res.status(500).json({ error: "Failed to load companies data." })
     }
 })
 
-app.get("/randomQuestions", (req: Request, res: Response) => {})
+app.get("/randomQuestions", (req: Request, res: Response) => { })
 
-app.post("/results", (req: Request, res: Response) => {})
+app.post("/results", (req: Request, res: Response) => { })
 
 // Serveren er på port 3000 (clienten er på port 5173)
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("Lytter til en port.")
 })
 
--- bedriftomat/src/pages/StartPage.tsx
+++ bedriftomat/src/pages/StartPage.tsx
@@ -2,27 +2,35 @@ import { useEffect, useState } from 'react';
 
 export default function StartPage() {
 
-    const [data, setData] = useState(null);
+    const [count, setCount] = useState(0);
+
+    const fetchCount = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/numCompanies');
+            const data = await response.json();
+            setCount(data.count);
+            console.log(count);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     useEffect(() => {
-        fetch('http://localhost:3000/numCompanies')
-            .then(response => response.json())
-            .then(data => setData(data))
-            .catch(error => console.error(error));
+        fetchCount();
     }, []);
 
-    const handleClick = () => {
-        fetch('http://localhost:3000/test')
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
-    }
+        const handleClick = () => {
+            fetch('http://localhost:3000/test')
+                .then(response => response.json())
+                .then(data => { console.log(data.text); })
+                .catch(error => console.error(error));
+        }
 
     return (
         <div className="flex flex-col justify-center max-w-lg">
             <h1>Tittel: Bedriftomat</h1>
             <p>Info om bedriftomaten...</p>
-            {data && <p>Antall bedrifter: {data.count}</p>}
+            {count && <p>Antall bedrifter: {count}</p>}
             <button onClick={handleClick}>Start</button>
         </div>
     )
