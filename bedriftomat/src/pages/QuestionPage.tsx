b52170a80bcc48b66dcb3b64a2f46a28fb74e348
Åmund <aamgrim@gmail.com>
2025-02-07 15:34:32 +0100

    example implementation

--- bedriftomat/src/pages/QuestionPage.tsx
+++ bedriftomat/src/pages/QuestionPage.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react"
+
 export default function QuestionPage() {
+
+    [data, setData] = useState([])
+    [result,setresult] = useState([zeros*companies.length])
+
+    useEffect(() => {
+    fetch("getQuestions") {
+        .then(response => response.json())
+        .then(data => {
+            
+        })
+    }, []}
+
     return(
         <p>Her svarer du på spørsmålene.</p>
+        <QuestionBox />
+        <AnswerBox />
+        <AnswerBox />
+        <AnswerBox  />
+        <AnswerBox />
+        <AnswerBox />
+        <NextButton />
     )
 }
