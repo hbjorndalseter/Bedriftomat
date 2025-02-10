2f27c61208ccf617b98b9e32ebe59b4765f56147
Åmund <aamgrim@gmail.com>
2025-02-10 19:17:30 +0100

    Color change when click

--- bedriftomat/src/components/AnswerButton.tsx
+++ bedriftomat/src/components/AnswerButton.tsx
@@ -7,6 +7,7 @@ const AnswerButton: React.FC<AnswerButtonProps> = ({ text, onClick, isSelected =
       <button
         className="flex justify-center items-center w-[335px] h-[18vw] max-h-[81px] bg-[#d9d9d9] rounded-[10px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"
         onClick={onClick}
+        style={{ backgroundColor: isSelected ? "#f26321" : "#d9d9d9", color: isSelected ? "white" : "black", transition: "background-color 0.3s" }}
       >
         <p className="w-9/10">{text}</p>
       </button>
