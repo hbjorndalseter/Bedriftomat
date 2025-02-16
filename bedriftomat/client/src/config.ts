4d570ae4f6eebcef7b1cd83a0e0c1527a7794820
hjalmabj <hjalmabj@stud.ntnu.no>
2025-02-17 00:12:57 +0100

    removed url in baseUrl

--- bedriftomat/client/src/config.ts
+++ bedriftomat/client/src/config.ts
@@ -1,6 +1,6 @@
 // You can also use an environment variable loader if needed.
 const baseUrl = process.env.NODE_ENV === 'production'
-  ? 'https://bedriftomat-ax4u.onrender.com' // In production, the client and server share the same domain
+  ? '' // In production, the client and server share the same domain
   : 'http://localhost:3000'; // In development, the server runs separately
 
 export default {
