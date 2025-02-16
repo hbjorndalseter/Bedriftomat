67fd2b51907208df206288233e19abf75e16ad9e
hjalmabj <hjalmabj@stud.ntnu.no>
2025-02-16 23:01:09 +0100

    image fetching

--- bedriftomat/server/index.ts
+++ bedriftomat/server/index.ts
@@ -12,10 +12,9 @@ app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
+app.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
-
 
  //Test endepunkt
  app.get("/test", (req: Request, res: Response) => {
