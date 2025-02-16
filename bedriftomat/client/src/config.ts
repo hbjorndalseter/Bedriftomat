1b73d48c437dd8c5a60cea4a20bf104708bc34f9
hjalmabj <hjalmabj@stud.ntnu.no>
2025-02-16 23:56:18 +0100

    changed url in config

--- bedriftomat/client/src/config.ts
+++ bedriftomat/client/src/config.ts
@@ -1,6 +1,6 @@
 // You can also use an environment variable loader if needed.
 const baseUrl = process.env.NODE_ENV === 'production'
-  ? '' // In production, the client and server share the same domain
+  ? 'https://bedriftomat-ax4u.onrender.com/' // In production, the client and server share the same domain
   : 'http://localhost:3000'; // In development, the server runs separately
 
 export default {
